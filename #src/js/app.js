f65e9860ac63275d0ff287e22105596fc989078b
Islam <isxmad@gmail.com>
2022-04-07 04:42:48 +0300

    first

---
+++ #src/js/app.js
@@ -0,0 +1,10 @@
+import { dropdown } from './modules/dropdown.js';
+import * as flsFuncs from './modules/functions.js';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    flsFuncs.isWebp();
+
+    // dropdown();
+})
